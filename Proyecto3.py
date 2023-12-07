1ea5ddee0f08f6d259b822bfd0011a8e03a302a3
JuanSarS <98623401+JuanSarS@users.noreply.github.com>
2023-12-06 23:31:08 -0500

    AAAAAAA

--- Proyecto3.py
+++ Proyecto3.py
@@ -1,64 +1,24 @@
 from sys import stdin, stdout
 import itertools
+import queue
 
-
-def es_clique(grafo, conjunto):
-    conjunto = list(conjunto)  # Convert the set to a list for indexing
-
+def es_clique_l(grafo : list, vecinos: list):
+    base=vecinos.copy()
     # Use a single loop to iterate over pairs of nodes
-    for i in range(len(conjunto)):
-        for j in range(i + 1, len(conjunto)):
-            nodo1 = conjunto[i]
-            nodo2 = conjunto[j]
+    for i in range(len(vecinos)):
+        actual= vecinos.pop()
 
-            if grafo[nodo1][nodo2] == 0:
-                return False
-
+        graf= grafo[actual]
+        for vecino in base: 
+            if vecino != actual: 
+             if vecino not in graf :
+                return False  
     return True
-
 
 def total_valor_clique(valores, conjunto):
     return sum(valores[nodo] for nodo in conjunto)
 
-
-def encontrar_clique_max_valor(grafo, valores, tamano_clique, conjunto_actual, nodos_restantes, max_valor, max_clique):
-
-    if len(conjunto_actual) > tamano_clique:
-        valor_actual = total_valor_clique(valores, conjunto_actual)
-        if valor_actual > max_valor:
-            max_valor = valor_actual
-            max_clique = conjunto_actual.copy()
-
-    for nodo in nodos_restantes:
-        nuevo_conjunto = conjunto_actual.copy()
-        nuevo_conjunto.add(nodo)
-
-        new_remaining = {n for n in nodos_restantes if grafo[nodo][n]}
-
-        if es_clique(grafo, nuevo_conjunto):
-            clique_valor = total_valor_clique(valores, nuevo_conjunto)
-            if clique_valor > max_valor:
-                max_valor = clique_valor
-                max_clique = nuevo_conjunto.copy()
-
-            temp_clique, temp_valor = encontrar_clique_max_valor(
-                grafo, valores, tamano_clique, nuevo_conjunto, new_remaining, max_valor, max_clique)
-
-            if temp_valor > max_valor:
-                max_valor = temp_valor
-                max_clique = temp_clique.copy()
-
-    return max_clique, max_valor
-
-
-def crear_grafo(grafo, vertices, personas):
-    for a in range(personas):
-        for b in vertices[a]:
-            grafo[a][b - 1] = 1
-    return grafo
-
-
-def main():
+def main2():
     N = int(stdin.readline())
     for i in range(N):
         # print(i)
@@ -66,18 +26,59 @@ def main():
         pagoPersonas = list(map(str.strip, pagoPersonas))
         pagoPersonas = list(map(int, pagoPersonas))
         personas = len(pagoPersonas)
-        vertices = []
+        grafo = []
+        grados=[]
+        cola= queue.PriorityQueue(personas)
         for i in range(personas):
             line = stdin.readline().strip()
             if line:
-                vertices.append(list(map(int, line.split(" "))))
-        grafo = [[0] * personas for z in range(personas)]
-        grafo = crear_grafo(grafo, vertices, personas)
-        conjunto_vacio = set()
-        nodos = [i for i in range(personas)]
-        max_valued_clique = encontrar_clique_max_valor(
-            grafo, pagoPersonas, 0, conjunto_vacio, nodos, 0, set())
-        print("Clique with max total value:", max_valued_clique)
+                vecinos= list(map(int, line.split(" ")))
+                vecinos=[n-1 for n in vecinos]
+                grado= len(vecinos)
+                grados.append(grado)
+                cola.put((grado,i))
+                grafo.append(vecinos)
+        n=[]
+        for i in range(cola.qsize()):
+           grad,nodo=cola.get()
+           n.append(nodo) 
 
+        sol=back_Track_kindof(n,grafo,[],grados)    
+        
+        clique=get_max(sol)
+        strr= str(total_valor_clique(pagoPersonas,clique)) + " "
+        for i in range(len(clique)):
+            if i == len(clique)-1:
+                strr+= str(clique(i)) + " "
+            else:
+                strr+= str(clique(i))          
+        print(strr)
 
-main()
+def get_max(sol:list):
+    ret= []
+    maxi=0
+    for sols in sol:
+        if len(sols)> maxi:
+           ret= sols 
+           maxi= len(sols)
+    return ret       
+
+def back_Track_kindof(n:list,grafo:list,sol:list,grados:list):
+    while n: 
+        actual = n.pop(0)
+        vecinos = grafo[actual]
+        grado= len(vecinos)
+        add=[]
+ 
+        for vecino in vecinos:
+            if grados[vecino]>= grado : 
+            
+              add.append(vecino)
+        add.append(actual)     
+            
+        if es_clique_l(grafo,add.copy()):
+         
+            sol.append(add)
+    return sol       
+    
+main2()
